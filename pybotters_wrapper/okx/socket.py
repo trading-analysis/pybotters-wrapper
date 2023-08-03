44f299412c99c0a2d8c63c433f6cff129b1ec7c3
richwomanbtc <richwomanbtc@gmail.com>
2023-08-03 18:52:28 +0400

    fix endpoint

--- pybotters_wrapper/okx/socket.py
+++ pybotters_wrapper/okx/socket.py
@@ -4,7 +4,8 @@ from pybotters_wrapper.core import WebsocketChannels
 
 
 class OKXWebsocketChannels(WebsocketChannels):
-    ENDPOINT = "wss://ws.okx.com:8443/ws/v5"
+    PUBLIC_ENDPOINT = "wss://ws.okx.com:8443/ws/v5/public"
+    PRIVATE_ENDPOINT = "wss://ws.okx.com:8443/ws/v5/private"
     INST_TYPE = "SWAP"
 
     def subscribe(self, channel: str, *args, **kwargs) -> OKXWebsocketChannels:
@@ -14,37 +15,39 @@ class OKXWebsocketChannels(WebsocketChannels):
         return {"op": "subscribe", "args": [{"channel": channel, **kwargs}]}
 
     def make_subscribe_endpoint(self, channel: str, *args, **kwargs) -> str:
-        # TODO: private endpoint
-        return self.ENDPOINT
+        if channel in ["orders", "positions"]:
+            return self.PRIVATE_ENDPOINT
+        return self.PUBLIC_ENDPOINT
 
     def ticker(self, symbol: str, **kwargs) -> OKXWebsocketChannels:
         return self.tickers(symbol)
 
     def trades(self, symbol: str, **kwargs) -> OKXWebsocketChannels:
-        return self.subscribe("public/trades", instId=symbol)
+        return self.subscribe("trades", instId=symbol)
 
     def orderbook(self, symbol: str, **kwargs) -> OKXWebsocketChannels:
         return self.books(symbol)
 
     def tickers(self, symbol: str) -> OKXWebsocketChannels:
-        return self.subscribe("public/tickers", instId=symbol)
+        return self.subscribe("tickers", instId=symbol)
 
     def books(self, symbol: str) -> OKXWebsocketChannels:
-        return self.subscribe("public/books", instId=symbol)
+        return self.subscribe("books", instId=symbol)
 
     def order(self, symbol: str, **kwargs) -> OKXWebsocketChannels:
         return self.subscribe(
-            "private/orders", symbol=symbol, instType=self.INST_TYPE, **kwargs
+            "orders", symbol=symbol, instType=self.INST_TYPE, **kwargs
         )
 
     def position(self, symbol: str, **kwargs) -> OKXWebsocketChannels:
         return self.subscribe(
-            "private/positions", symbol=symbol, instType=self.INST_TYPE, **kwargs
+            "positions", symbol=symbol, instType=self.INST_TYPE, **kwargs
         )
 
 
 class OKXTESTWebsocketChannels(OKXWebsocketChannels):
-    ENDPOINT = "wss://ws.okx.com:8443/ws/v5"
+    PUBLIC_ENDPOINT = "wss://ws.okx.com:8443/ws/v5/public?brokerId=9999"
+    PRIVATE_ENDPOINT = "wss://ws.okx.com:8443/ws/v5/private?brokerId=9999"
     INST_TYPE = "SWAP"
 
     def subscribe(self, channel: str, *args, **kwargs) -> OKXTESTWebsocketChannels:
